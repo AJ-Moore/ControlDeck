e6f37c9dd8c4c0cd57a2970b04ad1991adf59bc7
AJ-Moore <AJ_Moore@unorthodoxgamestudios.co.uk>
2015-06-18 10:52:05 +0100

    Work in progress... Added 151 function definitions in "CPU.h"
    Signed-off-by: AJ-Moore <AJ_Moore@unorthodoxgamestudios.co.uk>

--- source/CPU.cpp
+++ source/CPU.cpp
@@ -7,13 +7,13 @@
 
 namespace ControlDeck{
 
-	CPU::CPU(){
+	CPU::CPU():STACK(0x0100), PRGROM_UPPER(0xC000),PRGROM_LOWER(0x8000){
 
-		//!< The 6502 uses a 16 bit address bus $0 - $FFFF ($FFFF+1 possible addresses)
-		this->RAM = new UByte[0xFFFF+1];
+		//!< The 6502 uses a 16 bit address bus $0 - $FFFF ($FFFF+1 possible addresses) 
+		this->RAM = new UByte[0xFFFF]; //!< err $FFFF invalid, wrap to 0?
 
 		//!< Initialise RAM to NAUGHT
-		for (U16 i = 0; i < 0xFFFF+1; i++){
+		for (U16 i = 0; i < 0xFFFF; i++){
 			this->RAM[i] = 0; 
 		}
 
@@ -62,6 +62,11 @@ namespace ControlDeck{
 /** CPU MEMORY READ & WRITE **/
 
 	void CPU::writeMemory8(U16 Addr, U8 Data){
+
+		//!< Not sure what to do here yet -> just so I don't forget
+		if (Addr == 0xFFFF){
+			//throw eMAOOR;
+		}
 
 		//!< * Memory at $000-$07FF mirrored at $0800, $1000, $1800
 		//!< * Assuming any data written to range $0800-$2000 should
@@ -118,7 +123,7 @@ namespace ControlDeck{
 
 /** ======================= **/
 
-#pragma region _6502_Instructions 
+//#pragma region _6502_Instructions 
 
 /***Implied Addressing Mode Instructions***/
 	void CPU::BRK_$00(){
@@ -259,8 +264,113 @@ namespace ControlDeck{
 		(this->A & (1<<7)) ? this->P |= PFLAGS::NEGATIVE : this->P &= ~ PFLAGS::NEGATIVE;
 	}
 
+/**Add memory to accumulator**/
+	/*Add memory to Accumulator with carry*/
+	void CPU::ADC(U16 _M){
 
-#pragma endregion 151 instructions/ operation codes used by the 6502 
+		//!< Clear the carry flag 
+		//this->CLC_$18(); I'm not sure why this made sense to me
+
+		//!< Result 
+		U16 _R = (this->P & PFLAGS::CARRY) + this->A + _M;	
+
+		//!< Makes sence obviously presuming _M unsigned
+		//!< I hope this is right D:
+		((this->A & PFLAGS::NEGATIVE) & (_R & PFLAGS::NEGATIVE))
+			? P |= PFLAGS::OVER_FLOW : P & ~(PFLAGS::OVER_FLOW) ;
+
+		//!< So basically if zero
+		(_R == 0) ? P |= PFLAGS::ZERO : 
+					P & ~(PFLAGS::ZERO);
+
+		//!< check whether to set carry 
+		(_R > 255) ? this->P |= PFLAGS::CARRY :
+					 this->P &~ PFLAGS::CARRY;
+
+		//!< Set Accumulator 
+		A = _R;
+	}
+	void CPU::ADC_$69(){
+		//!< Immediate 
+		this->PC++; 
+		U16 _M = this->readMemory8(this->PC);
+
+		this->ADC(_M);
+
+		this->PC++;
+
+	}
+	void CPU::ADC_$65(){//!<Zero page 
+		this->PC++; 
+		U16 _M = this->readMemory8(this->readMemory8(this->PC));
+		this->ADC(_M);
+		this->PC++;
+	}
+	void CPU::ADC_$75(){//!< Indexed Zero Page, X	
+		this->PC++; 
+		U16 _Adr = this->readMemory8(this->PC);
+		//!< wrap
+		_Adr = (_Adr > 255) ? _Adr-255 : _Adr;
+				   
+		U16 _M = this->readMemory8(_Adr);
+		this->ADC(_M);
+		this->PC++;
+	} 
+	void CPU::ADC_$6D(){//!< Absolute (16 bit adr)
+		this->PC++; 
+		U16 _Adr = this->readMemory8(this->PC); 
+		_Adr &= (this->readMemory8(this->PC+1) << 8); 
+
+		U16 _M = this->readMemory8(_Adr); 
+		this->ADC(_M);
+		this->PC += 2;
+	}
+	void CPU::ADC_$7D(){ //!< Indexed Absolute X (16 bit adr)
+		this->PC++; 
+		U16 _Adr = this->readMemory8(this->PC); 
+		_Adr &= (this->readMemory8(this->PC+1) << 8); 
+		_Adr += this->X;
+
+		U16 _M = this->readMemory8(_Adr); 
+		this->ADC(_M);
+		this->PC += 2;
+	}
+	void CPU::ADC_$79(){ //!< Indexed Absolute Y (16 bit adr)
+		this->PC++; 
+		U16 _Adr = this->readMemory8(this->PC); 
+		_Adr &= (this->readMemory8(this->PC+1) << 8); 
+		_Adr += this->Y;
+
+		U16 _M = this->readMemory8(_Adr); 
+		this->ADC(_M);
+		this->PC += 2;
+	}
+	void CPU::ADC_$61(){ //!< Indexed Indirect X 
+		this->PC++; 
+		U16 _Adr = this->readMemory8(this->PC); 
+		_Adr &= (this->readMemory8(this->PC+1) << 8); 
+		_Adr += this->X;
+
+		U16 _AdrIndirect = this->readMemory8(_Adr); 
+		_AdrIndirect &= (this->readMemory8(_Adr+1) << 8); 
+
+		U16 _M = this->readMemory8(_AdrIndirect); 
+		this->ADC(_M);
+		this->PC += 2;
+	}
+	void CPU::ADC_$71(){ //!< Indirect Indixed Y
+		this->PC++; 
+		U16 _Adr = this->readMemory8(this->PC); 
+
+		U16 _AdrIndirect = this->readMemory8(_Adr); 
+		_AdrIndirect &= (this->readMemory8(_Adr+1) << 8); 
+
+		U16 _M = this->readMemory8(_AdrIndirect + this->Y); 
+		this->ADC(_M);
+		this->PC++;
+	}
+
+//#pragma endregion 151 instructions/ operation codes used by the 6502 
 
 // Helper Functions 
 	void CPU::incrementSP(){
@@ -270,4 +380,42 @@ namespace ControlDeck{
 	void CPU::decrementSP(){
 		(this->SP == 0) ? this->SP = 0xFF : this->SP--; 
 	}
+
+	//!< Establishes correct Addr location, 
+	U8 CPU::setAddrMode(U8 AddrMode){
+
+		switch (AddrMode){
+		case ADR_M::ZERO_PAGE:
+			break; 
+		case ADR_M::ZERO_PAGEX:
+			break; 
+		case ADR_M::ZERO_PAGEY:
+			break;
+		case ADR_M::ABSOLUTE:
+			break; 
+		case ADR_M::ABSOLUTEY:
+			break; 
+		case ADR_M::ABSOLUTEX:
+			break; 
+		case ADR_M::INDIRECT:
+			break; 
+		case ADR_M::IMPLIED:
+			break; 
+		case ADR_M::ACCUMULATOR:
+			break; 
+		case ADR_M::IMMEDIATE:
+			break; 
+		case ADR_M::RELATIVE:
+			break; 
+		case ADR_M::INDEXED_INDIRECT:
+			break; 
+		case ADR_M::INDIRECT_INDEXED:
+			break; 
+		default:
+			throw ("Invalid Address Mode");
+		}
+
+
+	}
+
 }
